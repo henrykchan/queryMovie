1f4b1c7d912043cfbdae5e7792976a6a35cc6f81
henrykchan <chan.henryk@gmail.com>
2017-01-03 21:20:51 -0500

    Created Project

---
+++ queryMovieProject/queryMovieProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  queryMovieProject
+//
+//  Created by Henry Chan on 1/3/17.
+//  Copyright © 2017 Henry Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
