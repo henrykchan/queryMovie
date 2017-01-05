70677c2439ade2d0e6badf2b062c1c4b1558d4ce
henrykchan <chan.henryk@gmail.com>
2017-01-04 19:20:23 -0500

    Start collectionviewcell

--- queryMovieProject/queryMovieProject/mainCollectionViewCell.swift
+++ queryMovieProject/queryMovieProject/mainCollectionViewCell.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2017 Henry Chan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell, UICollectionViewDelegate {
+    
+    var blurredPosterImageView = UIImageView()
+    var smallPosterImageView = UIImageView()
+    var titleLabel = UILabel()
+    var releaseDateLabel = UILabel()
+    var ratingLabel = UILabel()
+    var blackOverlay = UIView()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(blurredPosterImageView)
+        blurredPosterImageView.backgroundColor = UIColor.yellow
+        blurredPosterImageView.layer.borderWidth = 1
+        blurredPosterImageView.layer.masksToBounds = true
+        blurredPosterImageView.snp.makeConstraints { (make) in
+            make.centerX.equalTo(contentView.snp.centerX)
+            make.centerY.equalTo(contentView.snp.centerY)
+            make.width.equalTo(contentView.snp.width)
+            make.height.equalTo(contentView.snp.height)
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
