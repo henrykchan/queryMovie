02b5e13099604573a0b0621c77bcab80f617ec44
henrykchan <chan.henryk@gmail.com>
2017-01-05 23:31:16 -0500

    Finish Layout

---
+++ queryMovieProject/queryMovieProject/MainViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  queryMovieProject
+//
+//  Created by Henry Chan on 1/3/17.
+//  Copyright © 2017 Henry Chan. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var mainCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpMainCollectionViewCell()
+        createLayout()
+        
+        
+    }
+    
+    
+    // Create Layout
+    func createLayout() {
+        
+        view.backgroundColor = UIColor.white
+        view.addSubview(mainCollectionView)
+        mainCollectionView.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.top)
+            make.bottom.equalTo(view.snp.bottom)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+        }
+    }
+    
+    // Setup Cells
+    func setUpMainCollectionViewCell() {
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        //setup Layout
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = UICollectionViewScrollDirection.vertical
+        layout.itemSize = CGSize(width: screenWidth, height: screenHeight/4)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 0
+        mainCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        mainCollectionView.dataSource = self
+        mainCollectionView.delegate = self
+        
+        mainCollectionView.register(MainCollectionViewCell.self, forCellWithReuseIdentifier: "basicCell")
+        mainCollectionView.isUserInteractionEnabled = true
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "basicCell", for: indexPath) as! MainCollectionViewCell
+        
+        return cell
+    }
+    
+    
+
+    
+
+}
+
