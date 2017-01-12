886123ad892a5db15c0d7ba4c8947556b99c6496
henrykchan <chan.henryk@gmail.com>
2017-01-11 20:14:52 -0500

    minor edits 2

---
+++ queryMovieProject/queryMovieProject/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  queryMovieProject
+//
+//  Created by Henry Chan on 1/11/17.
+//  Copyright © 2017 Henry Chan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var contentView: UIView!
+    let infoTableView = UITableView()
+    let backgroundPosterImage = UIImageView()
+    let smallPosterImage = UIImageView()
+    let titleLabel = UILabel()
+    let ratingLabel = UILabel()
+    let blurredOverlay = UIView()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    
+
+    
+
+}
