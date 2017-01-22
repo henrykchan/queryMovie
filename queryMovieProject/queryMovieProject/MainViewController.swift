5bb74ab43f83cffe1502030c4a4baf177d7af2dc
henrykchan <chan.henryk@gmail.com>
2017-01-21 22:58:26 -0500

    reverting back to orginal search bar

--- queryMovieProject/queryMovieProject/MainViewController.swift
+++ queryMovieProject/queryMovieProject/MainViewController.swift
@@ -45,31 +45,31 @@ class MainViewController: UIViewController, UICollectionViewDelegateFlowLayout, 
         
         view.backgroundColor = UIColor.black
         
-//        self.view.addSubview(searchController.searchBar)
-//        searchController.searchBar.barTintColor = UIColor.black
-//        searchController.searchBar.snp.makeConstraints { (make) in
-//            make.top.equalTo(view.snp.top)
-//            make.centerX.equalTo(view.snp.centerX)
-//            make.height.equalTo(view.snp.height).dividedBy(20)
-//            make.width.equalTo(view.snp.width).multipliedBy(0.95)
-//        }
-//
-//        
-//        self.view.addSubview(mainCollectionView)
-//        mainCollectionView.snp.makeConstraints { (make) in
-//            make.top.equalTo(searchController.searchBar.snp.bottom)
-//            make.bottom.equalTo(view.snp.bottom)
-//            make.left.equalTo(view.snp.left)
-//            make.right.equalTo(view.snp.right)
-//        }
+        self.view.addSubview(searchController.searchBar)
+        searchController.searchBar.barTintColor = UIColor.black
+        searchController.searchBar.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.top)
+            make.centerX.equalTo(view.snp.centerX)
+            make.height.equalTo(view.snp.height).dividedBy(20)
+            make.width.equalTo(view.snp.width).multipliedBy(0.95)
+        }
+
         
         self.view.addSubview(mainCollectionView)
         mainCollectionView.snp.makeConstraints { (make) in
-            make.top.equalTo(view.snp.top)
+            make.top.equalTo(searchController.searchBar.snp.bottom)
             make.bottom.equalTo(view.snp.bottom)
             make.left.equalTo(view.snp.left)
             make.right.equalTo(view.snp.right)
         }
+        
+//        self.view.addSubview(mainCollectionView)
+//        mainCollectionView.snp.makeConstraints { (make) in
+//            make.top.equalTo(view.snp.top)
+//            make.bottom.equalTo(view.snp.bottom)
+//            make.left.equalTo(view.snp.left)
+//            make.right.equalTo(view.snp.right)
+//        }
         
         
         
@@ -108,9 +108,7 @@ class MainViewController: UIViewController, UICollectionViewDelegateFlowLayout, 
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.searchBar.placeholder = "Type Here..."
         searchController.searchBar.sizeToFit()
-//        let text = searchController.searchBar.text
         searchController.isActive = true
-//        searchController.searchBar.text = text
         self.searchController.extendedLayoutIncludesOpaqueBars = true
         searchController.searchBar.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
         
@@ -131,16 +129,16 @@ class MainViewController: UIViewController, UICollectionViewDelegateFlowLayout, 
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
-        header.backgroundColor = .blue
-        return header
-    }
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-//        return CGSize(width: view.frame.width, height: 12)
-//    }
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+////        
+////        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+////        header.backgroundColor = .blue
+////        return header
+////    }
+////    
+//////    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//////        return CGSize(width: view.frame.width, height: 12)
+//////    }
     
     func updateSearchResults(for searchController: UISearchController) {
         
