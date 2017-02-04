7398cfb576736cd3e35736c70bf4599c3a942031
henrykchan <chan.henryk@gmail.com>
2017-02-03 22:06:36 -0500

    minor edit

--- queryMovieProject/queryMovieProject/MainCollectionViewCell.swift
+++ queryMovieProject/queryMovieProject/MainCollectionViewCell.swift
@@ -89,6 +89,8 @@ class MainCollectionViewCell: UICollectionViewCell, UICollectionViewDelegate {
         self.titleLabel.text = selectedMovie.title
         self.yearLabel.text = selectedMovie.year
         self.ratingLabel.text = selectedMovie.imdbRating
+        
+        //maybe delete
 //        self.blurredPosterImageView = CustomImageView()
 //        self.smallPosterImageView = CustomImageView()
         
